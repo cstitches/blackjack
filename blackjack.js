c46bbcd6c896846a3094e90d74e294c1cebb358d
CE Dudley <cedudley@protonmail.com>
2021-12-06 09:44:24 -0800

    Create startRound and resetGame functions connected to buttons with event listeners.

---
+++ blackjack.js
@@ -0,0 +1,22 @@
+// VARIABLES: HTML ELEMENTS
+
+
+// Buttons & event listeners
+
+const startBtn = document.getElementById("start-btn")
+const resetBtn = document.getElementById("reset-btn")
+
+
+startBtn.addEventListener("click", startRound)
+resetBtn.addEventListener("click", resetGame)
+
+
+
+
+function startRound() {
+    console.log("Start round clicked")
+}
+
+function resetGame() {
+    //will reset all variables to initial values
+}
